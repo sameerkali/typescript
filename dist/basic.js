f085ad4cadaef55d3643c93d54c22f628422ba1b
Sameer Faridi <s.faridi007@gmail.com>
2023-04-27 17:48:56 +0530

    compleate all basics mosh is the best teacher on yt

--- dist/basic.js
+++ dist/basic.js
@@ -16,7 +16,69 @@ let numberr = [1, 2, 3, 4];
 let user = [69, 'sameer'];
 user[0].toExponential;
 user[1].replace;
+var Dize;
+(function (Dize) {
+    Dize[Dize["Small"] = 1] = "Small";
+    Dize[Dize["Medium"] = 2] = "Medium";
+    Dize[Dize["Large"] = 3] = "Large";
+})(Dize || (Dize = {}));
+;
 ;
 let MySize = 3;
 console.log(MySize);
+function incomeTaxCalculation(income, year) {
+    if (year < 2022)
+        return income * 1.2;
+    return income * 2;
+}
+console.log(incomeTaxCalculation(10000, 2020));
+function incomeTaxCalculate(income, year) {
+    if ((year || 2020) < 2022)
+        return income * 1.2;
+    return income * 2;
+}
+console.log(incomeTaxCalculate(10000));
+function TaxCalculate(income, year = 2020) {
+    if (year < 2022)
+        return income * 1.2;
+    return income * 2;
+}
+console.log(TaxCalculate(10000));
+let employee = {
+    id: 1,
+    name: 'sameer',
+    retire: (date) => { console.log(date); }
+};
+let employeee = {
+    id: 1,
+    name: 'sameer',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+console.log(kgToLbs(10));
+console.log(kgToLbs('10kg'));
+let quantity = 100;
+function greeting(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('undefined');
+}
+greeting('sameer');
+greeting(sameer);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customerr = getCustomer(1);
+if (customerr !== null && customerr !== undefined)
+    console.log(customerr.birthday);
+let costomerrr = getCustomer(1);
+console.log(costomerrr === null || costomerrr === void 0 ? void 0 : costomerrr.birthday);
 //# sourceMappingURL=basic.js.map
--- src/basic.ts
+++ src/basic.ts
@@ -34,7 +34,126 @@ user[1].replace;                                //use all string related functio
 
 /////////////////////////////////////////////////////[  ENUMS  ]////////////////////////////////////////////////////////////////////////////////////////////
 
-const enum Size {Small = 1, Medium, Large};
+enum Dize {Small = 1, Medium, Large};           //normal code
+const enum Size {Small = 1, Medium, Large};     // with const it is more optimise code 
 
 let MySize : Size = Size.Large;
 console.log(MySize);
+
+/////////////////////////////////////////////////////[  FUNCTONS  ]////////////////////////////////////////////////////////////////////////////////////////////
+
+function incomeTaxCalculation(income:number, year:number): number {
+    if(year < 2022)
+    return income*1.2;
+
+    return income*2;
+}
+console.log(incomeTaxCalculation(10_000, 2020));
+
+
+
+
+//type 2 -- ?
+
+function incomeTaxCalculate(income:number, year ?:number): number {        // use old js || operator
+    if((year || 2020) < 2022)
+    return income*1.2;
+
+    return income*2;
+}
+console.log(incomeTaxCalculate(10_000));
+
+
+
+//type 3 most optimised 
+
+function TaxCalculate(income:number, year = 2020): number {               // use predefine value
+    if(year < 2022)
+    return income*1.2;
+
+    return income*2;
+}
+console.log(TaxCalculate(10_000));
+
+/////////////////////////////////////////////////////[  OBJECTS  ]////////////////////////////////////////////////////////////////////////////////////////////
+
+let employee : {
+    readonly id : number,
+    name : string,
+    retire : (date:Date)=> void
+} = {
+    id : 1,
+    name : 'sameer',
+    retire : (date:Date) => { console.log(date)}
+};
+
+
+///////////////////////////////////////////[                        advance                     ]/////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////[  Type Alias  ]////////////////////////////////////////////////////////////////////////////////////////////
+
+type Employee = {
+    readonly id : number,
+    name : string,
+    retire : (date:Date) => void
+}
+
+
+let  /**/ employeee   /**/ : Employee = {
+    id : 1,
+    name : 'sameer',
+    retire : (date:Date) => {
+        console.log(date);
+    }
+}
+
+/////////////////////////////////////////////////////[  UNION TYPE  ]////////////////////////////////////////////////////////////////////////////////////////////
+
+function kgToLbs(weight : number | string) : number {
+    //narrowing
+    if(typeof weight === 'number')
+        return weight * 2.2;
+    else   
+        return parseInt(weight) * 2.2;
+}
+
+
+console.log(kgToLbs(10));
+console.log(kgToLbs('10kg'));
+
+/////////////////////////////////////////////////////[  LITERAL TYPE  ]////////////////////////////////////////////////////////////////////////////////////////////
+
+type Quantity = 50 | 100;
+let quantity : Quantity = 100;
+
+/////////////////////////////////////////////////////[  NULLABLE TYPE  ]////////////////////////////////////////////////////////////////////////////////////////////
+
+function greeting (name:string | null | undefined) {
+    if (name) console.log(name.toUpperCase());
+    // else if () console.log('hola');
+    else console.log('undefined')
+}
+
+greeting('sameer');
+greeting(sameer);
+
+/////////////////////////////////////////////////////[  OPTIONAL CHAINING  ]////////////////////////////////////////////////////////////////////////////////////////////
+
+type Customer = {
+    birthday : Date
+};
+
+function getCustomer(id:number) : Customer | null | undefined {
+    return id === 0 ? null : {birthday : new Date()}
+}
+
+
+// unoptimised way 
+let customerr = getCustomer(1);
+if (customerr !== null && customerr !== undefined)
+    console.log(customerr.birthday)
+
+// optimized way
+let costomerrr = getCustomer(1);
+// optional property access operation 
+console.log(costomerrr?.birthday)
